f6afbea6024a8a6e5f410b4be2ac8c75c481df5a
TheLucifer5372 <99461392+TheLucifer5372@users.noreply.github.com>
2022-11-30 19:47:40 +0530

    Added a POST API (Book)

--- index.js
+++ index.js
@@ -2,7 +2,21 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 require('dotenv').config();
+const booksRoute = require('./routes/books');
+
+
+
 const PORT = process.env.PORT || 3000
+
+
+//middlewares
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+//routes
+app.use('/api/books',booksRoute);
+
+
 
 //connect to Mongodb Atlas
 mongoose.connect(process.env.MONGO_URL,
---
+++ routes/books.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+const Book = require('../models/books');
+
+
+//POST : Create a New Book
+router.post("/", (req, res) => {
+
+    book = new Book({
+        name: req.body.bookName,
+        author: {
+            name: req.body.authorName,
+            age: req.body.authorAge
+        },
+        genre: req.body.genre
+    });
+
+    book.save().then(book => {
+        res.send(book);
+    }).catch(error => {
+        res.status(500).send("Book was not Stored in DB");
+    }); 
+
+});
+
+module.exports = router;
